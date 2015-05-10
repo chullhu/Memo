1f63bf37f1d0895fae9c48749466ddb5eabfa26e
Chtyllhu <d.a.shimanov@gmail.com>
2015-05-10 20:29:30 +0600

    Day One
    Добавлено в приложение:
    1. MainPage
    2. Шапка на главной странице
    3. Гамбургер меню
    4. В меню добавленно 3и кнопки
    5. Реализовано выезжание меню

---
+++ Memo/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Memo.Resources;
+
+namespace Memo
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Конструктор
+        public MainPage()
+        {
+            InitializeComponent();
+            DrawerLayout.InitializeDrawerLayout();
+        }
+
+        private void HambImg_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (DrawerLayout.IsDrawerOpen)
+                DrawerLayout.CloseDrawer();
+            else
+                DrawerLayout.OpenDrawer();
+        }
+
+        
+    }
+}
